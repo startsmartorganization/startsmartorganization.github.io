fadd210fe84689b262a87a874101832799839afe
NwayNwayThanHlaing <startsmart997@gmail.com>
2021-08-25 22:56:07 +0630

    workshop-update

--- js/main.js
+++ js/main.js
@@ -58,6 +58,40 @@
         time: 2000
     });
 
+        
+    // Causes carousel
+    $(".causes-carousel").owlCarousel({
+        autoplay: true,
+        animateIn: 'slideInDown',
+        animateOut: 'slideOutDown',
+        items: 1,
+        smartSpeed: 450,
+        dots: false,
+        loop: false,
+        responsive: {
+            0:{
+                items:1
+            },
+            576:{
+                items:1
+            },
+            768:{
+                items:2
+            },
+            992:{
+                items:3
+            }
+        }
+    });
+    
+    
+    // Causes progress
+    $('.causes-progress').waypoint(function () {
+        $('.progress .progress-bar').each(function () {
+            $(this).css("width", $(this).attr("aria-valuenow") + '%');
+        });
+    }, {offset: '80%'});
+    
 
     // Testimonials carousel
     $(".testimonials-carousel").owlCarousel({
